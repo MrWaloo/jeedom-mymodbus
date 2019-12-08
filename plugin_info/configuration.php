5f8a7221737090e8283fb8cd4fe658116e229959
Bebel27A <58235335+Bebel27A@users.noreply.github.com>
2019-12-08 13:43:59 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -25,15 +25,9 @@ if (!isConnect()) {
 <form class="form-horizontal">
     <fieldset>
         <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
+            <label class="col-lg-4 control-label">{{Port serie pour le mode RTU }}</label>
             <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param2" value="80" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
-            <div class="col-lg-2">
-                <select class="configKey form-control" data-l1key="param3">
+                <select class="configKey form-control" data-l1key="Portrtu">
                     <option value="value1">value1</option>
                     <option value="value2">value2</option>
                 </select>
