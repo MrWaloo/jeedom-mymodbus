b3de48a6351998b5d0a416fe6310743cca61e40a
Bebel27A <58235335+Bebel27A@users.noreply.github.com>
2021-10-24 22:02:28 +0200

    Update install_apt.sh
    Force pyserial 3.3 suite beug sur jeedom mgaloseau

--- ressources/install_apt.sh
+++ ressources/install_apt.sh
@@ -34,6 +34,7 @@ echo "Installation dependance  pypModbus 2.4.0"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 sudo pip3 uninstall pyModbus==2.5.0
 sudo pip3 install pyModbus==2.4.0
+sudo pip3 install pyserial==3.3
 echo 70 > ${PROGRESS_FILE}
 echo "-"
 #echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
