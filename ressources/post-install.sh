4c86e7d5533d9bdf3256dd24fd2e11bf35634bab
Michel Franke <michel.franke@gmail.com>
2023-03-14 23:24:02 +0100

    post-install.sh OK

--- ressources/post-install.sh
+++ ressources/post-install.sh
@@ -3,7 +3,8 @@ PROGRESS_FILE=/tmp/post-install_mymodbus_in_progress
 if [ ! -z $1 ]; then
 	PROGRESS_FILE=$1
 fi
-# Version of python into pyenv to install
+cd ../../plugins/mymodbus
+PYENV_ROOT="$(realpath ressources)/_pyenv"
 PYENV_VERSION="3.9.16"
 
 touch "$PROGRESS_FILE"
@@ -11,103 +12,103 @@ echo 0 > "$PROGRESS_FILE"
 echo "********************************************************"
 echo "*            Nettoyage de l'ancienne version           *"
 echo "********************************************************"
-if [ -f ../../plugins/mymodbus/core/php/mymodbus.inc.php ]; then
-    rm ../../plugins/mymodbus/core/php/mymodbus.inc.php
+if [ -f core/php/mymodbus.inc.php ]; then
+    rm core/php/mymodbus.inc.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/adam_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/adam_icon.png
+if [ -f desktop/images/adam_icon.png ]; then
+    rm desktop/images/adam_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/crouzet_m3_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/crouzet_m3_icon.png
+if [ -f desktop/images/crouzet_m3_icon.png ]; then
+    rm desktop/images/crouzet_m3_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/logo_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/logo_icon.png
+if [ -f desktop/images/logo_icon.png ]; then
+    rm desktop/images/logo_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/rtu_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/rtu_icon.png
+if [ -f desktop/images/rtu_icon.png ]; then
+    rm desktop/images/rtu_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/rtuovertcp_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/rtuovertcp_icon.png
+if [ -f desktop/images/rtuovertcp_icon.png ]; then
+    rm desktop/images/rtuovertcp_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/tcpip_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/tcpip_icon.png
+if [ -f desktop/images/tcpip_icon.png ]; then
+    rm desktop/images/tcpip_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/images/wago_icon.png ]; then
-    rm ../../plugins/mymodbus/desktop/images/wago_icon.png
+if [ -f desktop/images/wago_icon.png ]; then
+    rm desktop/images/wago_icon.png
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/adam.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/adam.configuration.php
+if [ -f desktop/modal/adam.configuration.php ]; then
+    rm desktop/modal/adam.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/crouzet_m3.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/crouzet_m3.configuration.php
+if [ -f desktop/modal/crouzet_m3.configuration.php ]; then
+    rm desktop/modal/crouzet_m3.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/logo.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/logo.configuration.php
+if [ -f desktop/modal/logo.configuration.php ]; then
+    rm desktop/modal/logo.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/rtu.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/rtu.configuration.php
+if [ -f desktop/modal/rtu.configuration.php ]; then
+    rm desktop/modal/rtu.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/rtuovertcp.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/rtuovertcp.configuration.php
+if [ -f desktop/modal/rtuovertcp.configuration.php ]; then
+    rm desktop/modal/rtuovertcp.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/tcpip.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/tcpip.configuration.php
+if [ -f desktop/modal/tcpip.configuration.php ]; then
+    rm desktop/modal/tcpip.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/templates.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/templates.php
+if [ -f desktop/modal/templates.php ]; then
+    rm desktop/modal/templates.php
 fi
-if [ -f ../../plugins/mymodbus/desktop/modal/wago.configuration.php ]; then
-    rm ../../plugins/mymodbus/desktop/modal/wago.configuration.php
+if [ -f desktop/modal/wago.configuration.php ]; then
+    rm desktop/modal/wago.configuration.php
 fi
-if [ -f ../../plugins/mymodbus/ressources/Biblio.zip ]; then
-    rm ../../plugins/mymodbus/ressources/Biblio.zip
+if [ -f ressources/Biblio.zip ]; then
+    rm ressources/Biblio.zip
 fi
-if [ -f ../../plugins/mymodbus/ressources/demon.py ]; then
-    rm ../../plugins/mymodbus/ressources/demon.py
+if [ -f ressources/demon.py ]; then
+    rm ressources/demon.py
 fi
-if [ -f ../../plugins/mymodbus/ressources/global.py ]; then
-    rm ../../plugins/mymodbus/ressources/global.py
+if [ -f ressources/global.py ]; then
+    rm ressources/global.py
 fi
-if [ -f ../../plugins/mymodbus/ressources/globals.py ]; then
-    rm ../../plugins/mymodbus/ressources/globals.py
+if [ -f ressources/globals.py ]; then
+    rm ressources/globals.py
 fi
-if [ -f ../../plugins/mymodbus/ressources/install_apt.sh ]; then
-    rm ../../plugins/mymodbus/ressources/install_apt.sh
+if [ -f ressources/install_apt.sh ]; then
+    rm ressources/install_apt.sh
 fi
-if [ -f ../../plugins/mymodbus/ressources/install.sh ]; then
-    rm ../../plugins/mymodbus/ressources/install.sh
+if [ -f ressources/install.sh ]; then
+    rm ressources/install.sh
 fi
-if [ -f ../../plugins/mymodbus/ressources/mymodbus_demond.py ]; then
-    rm ../../plugins/mymodbus/ressources/mymodbus_demond.py
+if [ -f ressources/mymodbus_demond.py ]; then
+    rm ressources/mymodbus_demond.py
 fi
-if [ -f ../../plugins/mymodbus/ressources/mymodbus_serv.py ]; then
-    rm ../../plugins/mymodbus/ressources/mymodbus_serv.py
+if [ -f ressources/mymodbus_serv.py ]; then
+    rm ressources/mymodbus_serv.py
 fi
-if [ -f ../../plugins/mymodbus/ressources/mymodbus_write.py ]; then
-    rm ../../plugins/mymodbus/ressources/mymodbus_write.py
+if [ -f ressources/mymodbus_write.py ]; then
+    rm ressources/mymodbus_write.py
 fi
-if [ -d ../../plugins/mymodbus/ressources/__pycache__ ]; then
-    rm -rf ../../plugins/mymodbus/ressources/__pycache__
+if [ -d ressources/__pycache__ ]; then
+    rm -rf ressources/__pycache__
 fi
-if [ -d ../../plugins/mymodbus/ressources/demond ]; then
-    rm -rf ../../plugins/mymodbus/ressources/demond
+if [ -d ressources/demond ]; then
+    rm -rf ressources/demond
 fi
-if [ -d ../../plugins/mymodbus/ressources/images ]; then
-    rm -rf ../../plugins/mymodbus/ressources/images
+if [ -d ressources/images ]; then
+    rm -rf ressources/images
 fi
-if [ -d ../../plugins/mymodbus/ressources/jeedom ]; then
-    rm -rf ../../plugins/mymodbus/ressources/jeedom
+if [ -d ressources/jeedom ]; then
+    rm -rf ressources/jeedom
 fi
 echo 5 > "$PROGRESS_FILE"
 echo "********************************************************"
 echo "*               Installation de pyenv                  *"
 echo "********************************************************"
 echo $(date)
-export PYENV_ROOT="$(realpath ../../plugins/mymodbus/ressources)/_pyenv"
 if [ ! -d "$PYENV_ROOT" ]; then
     sudo -E -u www-data curl https://pyenv.run | bash
     echo 20 > "$PROGRESS_FILE"
     echo "****  Configuration de pyenv..."
+    sudo -E -u www-data grep -vi pyenv ~www-data/.bashrc > ~www-data/.bashrc 2>/dev/null
     sudo -E -u www-data cat >> ~www-data/.bashrc<< EOF
 export PYENV_ROOT="$PYENV_ROOT"
 command -v pyenv >/dev/null || export PATH="\$PYENV_ROOT/bin:\$PATH"
@@ -126,7 +127,7 @@ echo 95 > "$PROGRESS_FILE"
 echo "********************************************************"
 echo "*      Configuration de pyenv avec python $PYENV_VERSION       *"
 echo "********************************************************"
-cd ../../plugins/mymodbus/ressources/mymodbusd
+cd ressources/mymodbusd
 chown -R www-data:www-data "$PYENV_ROOT"
 sudo -E -u www-data "$PYENV_ROOT"/bin/pyenv local "$PYENV_VERSION"
 sudo -E -u www-data "$PYENV_ROOT"/bin/pyenv exec pip install --upgrade pip setuptools
