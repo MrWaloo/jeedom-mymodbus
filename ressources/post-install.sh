1facdf17758df4d11c6051b582d5fd707b87a637
Michel Franke <michel.franke@gmail.com>
2023-11-28 15:50:49 +0100

    ## 28/11/2023 V2.0 beta26 - Correction du script post-install.sh pour mettre à jour pyenv avant d'installer python V3.11.6 (merci ngm47)

--- ressources/post-install.sh
+++ ressources/post-install.sh
@@ -149,6 +149,10 @@ if [ ! -d "$PYENV_ROOT/versions/$PYENV_VERSION" ]; then
   echo "*    Installation de python $PYENV_VERSION (dure longtemps)    *"
   echo "********************************************************"
   date
+  echo "**** Mise à jour de pyenv"
+  chown -R root:root "$PYENV_ROOT"
+  cd "$PYENV_ROOT" && git reset --hard && cd -
+  echo "**** Mise à jour de pyenv terminée, installation de python $PYENV_VERSION"
   chown -R www-data:www-data "$PYENV_ROOT"
   sudo -E -u www-data "$PYENV_ROOT"/bin/pyenv install "$PYENV_VERSION"
 fi
