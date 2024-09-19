4d41b764b125cc5822321336ef46f1ccc672b2dd
Michel F <80367602+MrWaloo@users.noreply.github.com>
2024-09-19 22:47:53 +0200

    backup

--- core/class/mymodbusConst.class.php
+++ core/class/mymodbusConst.class.php
@@ -6,11 +6,8 @@ class mymodbusConst {
   const PREFIX_TEMPLATE_PERSO   = '[Perso] ';
 
   const PATH_BACKUP             = '/data/backup/';
-  const PATH_TEMPLATES_PERSO    = '/data/template/';
-  const PATH_TEMPLATES_MYMODBUS = '/core/config/template/';
-
-  const PYENV_PYTHON            = '3.11.8';
-  const PYENV_SUFFIX            = 'pymodbus3.2.2';
+  const PATH_TEMPLATES_PERSO    = '/data/user_templates/';
+  const PATH_TEMPLATES_MYMODBUS = '/data/public_templates/';
 }
 
 ?>
