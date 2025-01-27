7e5e4703ad4317029f70b60f120c86d61801e378
Michel F <80367602+MrWaloo@users.noreply.github.com>
2025-01-27 16:56:19 +0100

    Update mymodbusd.py

--- resources/mymodbus/mymodbusd.py
+++ resources/mymodbus/mymodbusd.py
@@ -19,6 +19,7 @@ class MyModbusd(BaseDaemon):
     self.set_logger_log_level("MyModbus")
 
     self._mymodbus_clients: dict[str, MyModbusClient] = {}
+    self._mymodbus_test: MyModbusTest | None = None
     self._async_tasks: list[asyncio.Task] = []
     self.__n = self.__class__.__name__
 
@@ -121,8 +122,12 @@ class MyModbusd(BaseDaemon):
     ))
     new_client.read_eqConfig()
     new_client.connect()
-    self._logger.info(f"{self.__n}: Starting the task for the equipement {eqConfig['name']}")
-    self._mymodbus_clients[eqConfig["id"]] = new_client
+    if eqConfig["id"] != "test":
+      self._logger.info(f"{self.__n}: Starting the task for the equipement {eqConfig['name']}")
+      self._mymodbus_clients[eqConfig["id"]] = new_client
+    else:
+      self._logger.info(f"{self.__n}: Starting the task to test an equipement")
+      self._mymodbus_test = new_client
 
   async def terminate_client(self, eqId: str) -> None:
     await self.send_downstream(eqId, {"quit": None})
