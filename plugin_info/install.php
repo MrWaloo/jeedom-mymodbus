5a6426aa982cde13a70a26b441d1a37d1f660459
Michel F <80367602+MrWaloo@users.noreply.github.com>
2025-08-31 22:58:04 +0200

    pymodbus V3.11.1

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -104,12 +104,10 @@ function mymodbus_update() {
 	// This is necessary to update the configuration of the plugin
 	$eqLogics = eqLogic::byType($pluginId);
 	foreach ($eqLogics as $eqLogic) {
-		if ($eqLogic->getIsEnable()) {
-			$eqLogic->save();
-			foreach ($eqLogic->getCmd() as $cmdMymodbus) { // loop over the commands of the eqLogic
-				$cmdMymodbus->save();
-			}
+		foreach ($eqLogic->getCmd() as $cmdMymodbus) { // loop over the commands of the eqLogic
+			$cmdMymodbus->save();
 		}
+		$eqLogic->save();
 	}
 
 	delete_unused_files();
