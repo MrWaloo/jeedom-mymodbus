f198fe6283e744db9449a854b14aa04fab619b7f
Michel F <80367602+MrWaloo@users.noreply.github.com>
2025-09-01 22:14:27 +0200

    Correction V3.0.5

--- core/class/mymodbus.class.php
+++ core/class/mymodbus.class.php
@@ -1064,11 +1064,12 @@ class mymodbusCmd extends cmd {
 			'cmdSlave' => 'cmdDevID'
 		];
 		$old = cmd::byId($this->getId());
-		foreach ($conv_array as $oldKey => $newKey) {
-			if ($this->getConfiguration($oldKey, '') !== '') { // && $this->getConfiguration($newKey, '') === ''
-				$this->setConfiguration($newKey, $old->getConfiguration($oldKey));
-				$this->setConfiguration($oldKey, null);
-				$this->_changed = true;
+		if (!is_null($old)) {
+			foreach ($conv_array as $oldKey => $newKey) {
+				if ($old->getConfiguration($oldKey, '') !== '') {
+					$this->setConfiguration($newKey, $old->getConfiguration($oldKey));
+					$this->setConfiguration($oldKey, null);
+				}
 			}
 		}
 		
